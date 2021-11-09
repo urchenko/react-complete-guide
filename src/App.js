6162c42a11c120e7ee8a8b5ef038c01e37b475af
myurch <myurch@softserveinc.com>
2021-11-09 13:16:40 +0200

    useCallback() and its Dependencies

--- src/App.js
+++ src/App.js
@@ -7,19 +7,27 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToogle, setAllowToogle] = useState(false);
 
   console.log('APP RUNNING!');
 
   const toogleButtonHandler = useCallback(() => {
-    setShowParagraph((showParagraph) => !showParagraph);
-  }, []);
+    if (allowToogle) {
+      setShowParagraph((showParagraph) => !showParagraph);
+    }
+  }, [allowToogle]);
+
+  const allowToogleHandler = () => {
+    setAllowToogle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       {/* {showParagraph && <p>This is new!</p>} */}
       {/* <DemoOutput show={showParagraph} /> */}
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToogleHandler}>Allow Toogling!</Button>
       <Button onClick={toogleButtonHandler}>Toogle Paragraph!</Button>
     </div>
   );
