e1856b502ad84c4bcc2ab7180ed76f362b03d702
myurch <myurch@softserveinc.com>
2021-11-09 15:37:19 +0200

    Optimizing with useMemo()

--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
-import React, { useState, useCallback } from 'react';
-
-import Button from './components/UI/Button/Button';
-import DemoOutput from './components/Demo/DemoOutput';
+import React, { useState, useCallback, useMemo } from 'react';
 
 import './App.css';
+import DemoList from './components/Demo/DemoList';
+import Button from './components/UI/Button/Button';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToogle, setAllowToogle] = useState(false);
+  const [listTitle, setListTitle] = useState('My List');
 
-  console.log('APP RUNNING!');
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
 
-  const toogleButtonHandler = useCallback(() => {
-    if (allowToogle) {
-      setShowParagraph((showParagraph) => !showParagraph);
-    }
-  }, [allowToogle]);
-
-  const allowToogleHandler = () => {
-    setAllowToogle(true);
-  };
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      {/* {showParagraph && <p>This is new!</p>} */}
-      {/* <DemoOutput show={showParagraph} /> */}
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToogleHandler}>Allow Toogling!</Button>
-      <Button onClick={toogleButtonHandler}>Toogle Paragraph!</Button>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
 
 export default App;
+
+// import React, { useState, useCallback } from 'react';
+
+// import Button from './components/UI/Button/Button';
+// import DemoOutput from './components/Demo/DemoList';
+
+// import './App.css';
+
+// function App() {
+//   const [showParagraph, setShowParagraph] = useState(false);
+//   const [allowToogle, setAllowToogle] = useState(false);
+
+//   console.log('APP RUNNING!');
+
+//   const toogleButtonHandler = useCallback(() => {
+//     if (allowToogle) {
+//       setShowParagraph((showParagraph) => !showParagraph);
+//     }
+//   }, [allowToogle]);
+
+//   const allowToogleHandler = () => {
+//     setAllowToogle(true);
+//   };
+
+//   return (
+//     <div className="app">
+//       <h1>Hi there!</h1>
+//       {/* {showParagraph && <p>This is new!</p>} */}
+//       {/* <DemoOutput show={showParagraph} /> */}
+//       <DemoOutput show={showParagraph} />
+//       <DemoOutput title={showParagraph} />
+//       <Button onClick={allowToogleHandler}>Allow Toogling!</Button>
+//       <Button onClick={toogleButtonHandler}>Toogle Paragraph!</Button>
+//     </div>
+//   );
+// }
+
+// export default App;
