68133e10aba07849c85e916f072661d7442de71a
myurch <myurch@softserveinc.com>
2021-11-10 12:20:40 +0200

    Using async await instead

--- src/App.js
+++ src/App.js
@@ -20,22 +20,20 @@ function App() {
   // ];
   const [movies, setMovies] = useState([]);
 
-  function fetchMovieHandler() {
-    fetch('https://swapi.dev/api/films')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movie) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function fetchMovieHandler() {
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+
+    setMovies(transformedMovies);
   }
 
   return (
