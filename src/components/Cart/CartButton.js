bd4a648cb4a4ff9528cf7aba58c7388daa0218f0
myurch <myurch@softserveinc.com>
2021-11-14 18:01:09 +0200

    Add redux store interaction

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,24 @@
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+
+import { uiActions } from '../../store/ui-slice';
+
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const itemsInCart = useSelector((state) => state.cart.totalQuantity);
+
+  const toggleCartHandler = (event) => {
+    event.preventDefault();
+
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{itemsInCart}</span>
     </button>
   );
 };
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,22 @@
+import { useDispatch } from 'react-redux';
+
 import Card from '../UI/Card';
+import { cartActions } from '../../store/cart-slice';
+
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { id, title, price, description } = props;
+  const dispatch = useDispatch();
+
+  const addToCartHandler = () => {
+    const item = {
+      id,
+      price,
+      title,
+    };
+    dispatch(cartActions.addItemToCart(item));
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +27,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
