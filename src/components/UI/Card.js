22db1b727ca872e84f35082c260c8a75f7fdac5b
myurch <myurch@softserveinc.com>
2021-11-14 12:55:19 +0200

    Alinged changes

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,9 +1,13 @@
-import './Card.css';
+import classes from './Card.module.css';
 
 const Card = (props) => {
-  const classes = 'card ' + props.className;
-
-  return <div className={classes}>{props.children}</div>;
+  return (
+    <section
+      className={`${classes.card} ${props.className ? props.className : ''}`}
+    >
+      {props.children}
+    </section>
+  );
 };
 
 export default Card;
