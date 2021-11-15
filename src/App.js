c2c1849eb3874f16a2da4e195025d49b624ffaf5
myurch <myurch@softserveinc.com>
2021-11-15 12:35:03 +0200

    Basic routs setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { Route } from 'react-router-dom';
+import MainHeader from './components/MainHeader';
+
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <header></header>
+      <main>
+        <MainHeader />
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+// our domain/welcome => Welcome Component
+// our domain/products => Product Component
