1e30c6aef709ee6d9266da8def1399785e78166b
myurch <myurch@softserveinc.com>
2021-11-15 16:47:39 +0200

    Extracted param

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,17 @@
+import { Fragment } from 'react';
+import { useParams } from 'react-router-dom';
+
 const QuoteDetail = () => {
-  return <h1>Quote Detail Page</h1>;
+  const params = useParams();
+
+  console.log(params.quoteId);
+
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
