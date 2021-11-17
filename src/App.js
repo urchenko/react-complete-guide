6737151a9b453f07a1ad3051987a18c34124c01b
myurch <myurch@softserveinc.com>
2021-11-17 13:23:02 +0200

    alinged second example from 5 to 6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -10,11 +10,20 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Routes>
-          <Route path="/welcome" element={<Welcome />} />
-          <Route path="/products/*" element={<Products />} />
-          <Route path="/products/:productId" element={<ProductDetail />} />
-        </Routes>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/welcome' />
+          </Route>
+          <Route path='/welcome'>
+            <Welcome />
+          </Route>
+          <Route path='/products' exact>
+            <Products />
+          </Route>
+          <Route path='/products/:productId'>
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
