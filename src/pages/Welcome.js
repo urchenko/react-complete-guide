e53d281017522256cb8ae5e68a679b6d4fc09ffd
myurch <myurch@softserveinc.com>
2021-11-17 15:13:44 +0200

    second example from 5 to 6

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,12 +1,14 @@
-import { Route } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
-      <Route path="/welcome/new-user">
-        <p>Welcome, new user!</p>
-      </Route>
+      <Link to="new-user">New User</Link>
+      {/* <Routes>
+        <Route path="new-user" element={<p>Welcome, new user!</p>}></Route>
+      </Routes> */}
+      <Outlet />
     </section>
   );
 };
