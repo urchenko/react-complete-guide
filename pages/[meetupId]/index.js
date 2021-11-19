8279be375e994212c9617e3cd09e3583f35e8beb
myurch <myurch@softserveinc.com>
2021-11-19 16:29:44 +0200

    Using Programmatic (Imperative) Navigation

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      title="A Second Meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      address="Some address 20, 54321 Some City"
+      description="This is a second meetup!"
+    />
+  );
+}
+
+export default MeetupDetails;
