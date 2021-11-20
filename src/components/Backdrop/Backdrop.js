f4dc7c5caf68885087e23603e1ce35b8aef38a4e
myurch <myurch@softserveinc.com>
2021-11-20 17:35:23 +0200

    Using ReactTransitionGroup

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -3,9 +3,9 @@ import React from 'react';
 import './Backdrop.css';
 
 const backdrop = (props) => {
-  const cssClasses = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed'];
+    const cssClasses = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed'];
 
-  return <div className={cssClasses.join(' ')}></div>;
+    return <div className={cssClasses.join(' ')}></div>;
 };
 
 export default backdrop;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import './Modal.css';
+import "./Modal.css";
 
-const modal = (props) => {
-  const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
+const modal = props => {
+  const cssClasses = [
+    "Modal",
+    props.show ? "ModalOpen" : "ModalClosed"
+  ];
 
   return (
     <div className={cssClasses.join(' ')}>
