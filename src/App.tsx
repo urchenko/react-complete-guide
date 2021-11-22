b6d1e234b289608c6320ba180d51ba6ddb8649c7
myurch <myurch@softserveinc.com>
2021-11-22 18:17:17 +0200

    Form Submissions In TypeScript Projects

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import Todos from './components/Todos';
 import Todo from './models/todo';
 
 import './App.css';
+import NewTodo from './components/newTodo';
 
 function App() {
   const todos = [new Todo('Learn React'), new Todo('Learn React')];
 
+  const addTodoHandler = (todoText: string) => {};
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
