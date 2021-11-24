639de0c3b54f187f91bd5e765b0ea76c3f6e1396
myurch <myurch@softserveinc.com>
2021-11-24 14:17:50 +0200

    finished

--- src/components/newTodo.tsx
+++ src/components/newTodo.tsx
@@ -1,9 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useContext, useRef } from 'react';
+import { TodosContext } from '../store/todos-context';
 
 import classes from './NewTodo.module.css';
 
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
+  const todosCtx = useContext(TodosContext);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
@@ -15,7 +17,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
       return;
     }
 
-    props.onAddTodo(enteredText);
+    todosCtx.addTodo(enteredText);
   };
 
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TodosContextProvider from './store/todos-context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <TodosContextProvider>
+    <App />
+  </TodosContextProvider>,
+  document.getElementById('root')
+);
